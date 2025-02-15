5ce904b01bbae9850bc6d5dd209f08184b68738e
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-02-15 14:24:06 -0300

    :wrench: chore: add db context

---
+++ LibrarySystem/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LibrarySystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Data;
+
+public class LibraryDbContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<BookReservation> BookReservations { get; set; }
+
+    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+    { }
+}
+

ac88780ee3e9dc15b2ab12de0683265ba23b4ea6
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-02-15 15:59:21 -0300

    :wrench: chore: update db context name

--- LibrarySystem/Data/AppDbContext.cs
+++ LibrarySystem/Data/AppDbContext.cs
@@ -3,14 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LibrarySystem.Data;
 
-public class LibraryDbContext : DbContext
+public class AppDbContext : DbContext
 {
     public DbSet<Book> Books { get; set; }
     public DbSet<Author> Authors { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<BookReservation> BookReservations { get; set; }
 
-    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     { }
 }
 

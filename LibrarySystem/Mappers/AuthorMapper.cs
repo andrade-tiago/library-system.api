d35586c5892e39c75a7e521deaa4b13ebb804d8d
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-02-22 11:56:04 -0300

    :sparkles: feat: add mappers

---
+++ LibrarySystem/Mappers/AuthorMapper.cs
@@ -0,0 +1,16 @@
+﻿using LibrarySystem.DTOs.Author;
+using LibrarySystem.Models;
+
+namespace LibrarySystem.Mappers;
+
+public class AuthorMapper
+{
+    public static AuthorDto ToDto(Author author)
+    {
+        return new AuthorDto
+        {
+            Id = author.Id,
+            Name = author.Name,
+        };
+    }
+}

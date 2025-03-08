e9616d36ae711c196f8b2c3f99790d2f94274181
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-03-08 17:58:12 -0300

    :label: fix(ReservationDto): replace CreatedDate type

--- LibrarySystem/DTOs/Reservation/ReservationDto.cs
+++ LibrarySystem/DTOs/Reservation/ReservationDto.cs
@@ -8,7 +8,7 @@ public class ReservationDto
     public int Id { get; set; }
     public BookDto Book { get; set; }
     public CustomerDto Customer { get; set; }
-    public DateTime CreatedDate { get; set; }
+    public DateOnly CreatedDate { get; set; }
     public DateOnly EndDate { get; set; }
     public DateOnly? ReturnedDate { get; set; }
 }
